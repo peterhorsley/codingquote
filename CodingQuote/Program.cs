9608c63f3eb6c42ddbb89c4f4e1c6ace5d60efc3
Peter Horsley <peter.g.horsley@gmail.com>
2018-06-05 23:07:36 +1000

    Program to select a random coding-related quote and print it to the console.

---
+++ CodingQuote/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Resources;
+using System.Reflection;
+using System.IO;
+
+namespace CodingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quotes = new List<Quote>();
+
+            using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("CodingQuote.Quotes.txt")))
+            {
+                try
+                {
+                    while (reader.Peek() != -1)
+                    {
+                        quotes.Add(ReadQuote(reader));
+                        reader.ReadLine();
+                    }
+                }
+                catch (IOException) { }
+            }
+
+            var randomizer = new Random();
+            int chosenQuote = randomizer.Next(quotes.Count - 1);
+            Console.WriteLine(quotes[chosenQuote]);
+        }
+
+        private static Quote ReadQuote(StreamReader reader)
+        {
+            var text = reader.ReadLine();
+            var source = reader.ReadLine();
+            return new Quote(text, source);
+        }
+    }
+}
---
+++ CodingQuote/Quote.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingQuote
+{
+    public class Quote
+    {
+        public Quote(string text, string source)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentException("text");
+            }
+            if (String.IsNullOrWhiteSpace(source))
+            {
+                throw new ArgumentException("source");
+            }
+
+            Text = text;
+            Source = source;
+        }
+
+        public string Text { get; private set; }
+        public string Source { get; private set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\n{1}", Text, Source);
+        }
+    }
+}
